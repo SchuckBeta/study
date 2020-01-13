abb1b8eb46f92d225a790fc7c93471694354efef
陈浩 <chenhao@os-easy.com>
2020-01-13 10:34:41 +0800

    lambda

---
+++ 脚本工具/del-lastUpdated.bat
@@ -0,0 +1,9 @@
+
+@echo off
+set REPOSITORY_PATH=D:\m2_repository\repository1
+rem 正在搜索...
+for /f "delims=" %%i in ('dir /b /s "%REPOSITORY_PATH%\*lastUpdated*"') do (
+    del /s /q %%i
+)
+rem 搜索完毕
+pause
